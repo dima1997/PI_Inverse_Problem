fb9189efaed308b7e722bf765450a42423355a1b
dima1997 <franki.dimaria@gmail.com>
2024-02-28 00:35:37 +0100

    add: Gibbs Sampler

--- TP1/TP_inverse_problems.m
+++ TP1/TP_inverse_problems.m
@@ -12,8 +12,8 @@ Xs=G*S;
 [~,id]=max(mean(S,1));
 
 %visualize original source distribution
-figure; trisurf(mesh.f,mesh.v(:,1),mesh.v(:,2),mesh.v(:,3),S(:,id));
-title('original source configuration: two source regions','FontSize',18); axis off;
+##figure; trisurf(mesh.f,mesh.v(:,1),mesh.v(:,2),mesh.v(:,3),S(:,id));
+##title('original source configuration: two source regions','FontSize',18); axis off;
 
 %generate Gaussian random noise
 Noise=randn(size(Xs));
@@ -29,13 +29,30 @@ X=Xs+1/sqrt(SNR)*Noise;
 
 %visualize data (for a reduced number of sensors whose indices are
 %specified by idx_electrodes)
-plot_eeg(X(idx_electrodes,:),max(max(X(idx_electrodes,:))),256,channel_names);
-title('noisy EEG data','FontSize',18);
+##plot_eeg(X(idx_electrodes,:),max(max(X(idx_electrodes,:))),256,channel_names);
+##title('noisy EEG data','FontSize',18);
 
 % ------------ TP1 -----------------------------------
-%% Gibbs sampler
+##figure; trisurf(mesh.f,mesh.v(:,1),mesh.v(:,2),mesh.v(:,3),S(:,id));
+##title('Original source','FontSize',18); axis off;
 
-Shat = Gibbs_sampler(X(:,id),A);  %
+%% Gibbs sampler
+SNRs = [0.1]; % , 1, 10
+for i = 1:length(SNRs)
+    SNR = SNRs(i);
+    fprintf("SNR = %d\n", SNR);
+    X=Xs+1/sqrt(SNR)*Noise;
+    sigma_n2 = 3e3;
+    sigma_s2 = 20;
+    alpha    = 1.01;
+    beta     = 2e3;
+    NIter    = 4;
+    [Shat,Lambdahat] = Gibbs_sampler(X(:,id),G,sigma_n2, sigma_s2, alpha, beta, NIter);
+##    plot_eeg(X(idx_electrodes,:),max(max(X(idx_electrodes,:))),256,channel_names);
+##    title(sprintf('EEG data (SNR=%d)', SNR),'FontSize',18);
+    figure; trisurf(mesh.f,mesh.v(:,1),mesh.v(:,2),mesh.v(:,3),Shat);
+    title(sprintf('Reconstructed source (SNR=%d)', SNR),'FontSize',18); axis off;
+end
 
 % ------------ end of TP1 ----------------------------
 
@@ -43,12 +60,12 @@ Shat = Gibbs_sampler(X(:,id),A);  %
 % -----------  TP2 / 3 -------------------------------
 
 %% manual test of lambda
-lambda = logspace(0,3,6);
-for k=1:length(lambda)
-    Shat(:,k)=MNE(X(:,id),G,lambda(k));  % function to be implemented
-    figure; trisurf(mesh.f,mesh.v(:,1),mesh.v(:,2),mesh.v(:,3),Shat(:,k));
-    title(strcat('\lambda=',num2str(lambda(k))));
-end
+##lambda = logspace(0,3,6);
+##for k=1:length(lambda)
+##    Shat(:,k)=MNE(X(:,id),G,lambda(k));  % function to be implemented
+##    figure; trisurf(mesh.f,mesh.v(:,1),mesh.v(:,2),mesh.v(:,3),Shat(:,k));
+##    title(strcat('\lambda=',num2str(lambda(k))));
+##end
 
 
 %% L-curve criterion
@@ -66,8 +83,8 @@ end
 
 
 %% SISSY
-T=variation_operator(mesh,'face');
-lambda=logspace(-2,3,6); % lambda from 0.01 to 1000
-for k=1:length(lambda)
-[SI,lam]=SISSY(X(:,id),G,T,lambda(k),0.1, MaxIter);  %  function to be implemented
-end
+##T=variation_operator(mesh,'face');
+##lambda=logspace(-2,3,6); % lambda from 0.01 to 1000
+##for k=1:length(lambda)
+##[SI,lam]=SISSY(X(:,id),G,T,lambda(k),0.1, MaxIter);  %  function to be implemented
+##end
